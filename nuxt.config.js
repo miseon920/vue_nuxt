b919cb827caca9d0ee0a099315a66765efce8edc
kim mi seon <miseon920@gmail.com>
2023-08-10 16:13:17 +0900

    '메타태그변경'

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,14 +2,14 @@ export default {
   target:'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'vue_nuxt',
+    title: '넉스트 쇼핑',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ko',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '넉스트 쇼핑 예시' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
